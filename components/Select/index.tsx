dfa1b24d49ad87adc6a8b57975aba4737d9d64c5
ohene gyan <ohenegyan159@gmail.com>
2026-01-22 13:40:29 +0000

    Fix Select component type error for build

--- components/Select/index.tsx
+++ components/Select/index.tsx
@@ -38,7 +38,7 @@ const Select = ({
     return (
         <Listbox
             className={`${className || ""}`}
-            value={value}
+            value={value ?? undefined}
             onChange={onChange}
             as="div"
         >
@@ -49,15 +49,13 @@ const Select = ({
                 </Label>
             )}
             <ListboxButton
-                className={`group flex justify-between items-center w-full h-13 px-3 border border-gray-100 rounded-xl text-body-lg text-gray-900 fill-gray-400 transition-all outline-0 data-[hover]:border-primary-500 data-[hover]:fill-gray-900 data-[open]:fill-gray-900 data-[open]:border-primary-500 data-[open]:bg-primary-50 max-md:h-12 ${
-                    isMedium
+                className={`group flex justify-between items-center w-full h-13 px-3 border border-gray-100 rounded-xl text-body-lg text-gray-900 fill-gray-400 transition-all outline-0 data-[hover]:border-primary-500 data-[hover]:fill-gray-900 data-[open]:fill-gray-900 data-[open]:border-primary-500 data-[open]:bg-primary-50 max-md:h-12 ${isMedium
                         ? "!h-10 !pr-2 !rounded-[0.625rem] !text-body-md font-medium"
                         : ""
-                } ${
-                    isSmall
+                    } ${isSmall
                         ? "!h-8 !pr-2 !rounded-lg !text-body-sm font-medium"
                         : ""
-                } ${classButton || ""}`}
+                    } ${classButton || ""}`}
             >
                 {value?.name ? (
                     <div className="truncate">{value.name}</div>
@@ -65,43 +63,37 @@ const Select = ({
                     <div className="truncate text-gray-400">{placeholder}</div>
                 )}
                 <Icon
-                    className={`shrink-0 ml-2 fill-inherit -rotate-90 transition-transform group-[[data-open]]:rotate-90 ${
-                        isMedium || isSmall ? "!size-4" : ""
-                    }`}
+                    className={`shrink-0 ml-2 fill-inherit -rotate-90 transition-transform group-[[data-open]]:rotate-90 ${isMedium || isSmall ? "!size-4" : ""
+                        }`}
                     name="chevron"
                 />
             </ListboxButton>
             <ListboxOptions
-                className={`z-100 w-[var(--button-width)] bg-white border border-gray-100 shadow-lg rounded-xl origin-top transition duration-200 ease-out outline-none data-[closed]:scale-95 data-[closed]:opacity-0 ${
-                    isMedium || isSmall
+                className={`z-100 w-[var(--button-width)] bg-white border border-gray-100 shadow-lg rounded-xl origin-top transition duration-200 ease-out outline-none data-[closed]:scale-95 data-[closed]:opacity-0 ${isMedium || isSmall
                         ? "[--anchor-gap:0.25rem] p-1"
                         : "[--anchor-gap:0.5rem] p-2"
-                } ${isMedium ? "!rounded-[0.0625rem]" : ""} ${
-                    isSmall ? "!rounded-lg" : ""
-                }`}
+                    } ${isMedium ? "!rounded-[0.0625rem]" : ""} ${isSmall ? "!rounded-lg" : ""
+                    }`}
                 anchor="bottom"
                 transition
                 modal={false}
             >
                 {options.map((option) => (
                     <ListboxOption
-                        className={`group relative py-2.5 rounded-lg truncate text-body-lg font-medium text-gray-900 cursor-pointer transition-colors data-[focus]:bg-gray-25 data-[selected]:bg-gray-25 not-last:mb-0.5 ${
-                            isMedium || isSmall
+                        className={`group relative py-2.5 rounded-lg truncate text-body-lg font-medium text-gray-900 cursor-pointer transition-colors data-[focus]:bg-gray-25 data-[selected]:bg-gray-25 not-last:mb-0.5 ${isMedium || isSmall
                                 ? "px-2 !rounded-md data-[selected]:pr-6"
                                 : "px-3 data-[selected]:pr-10.5"
-                        } ${isMedium ? "!py-1.5 !text-body-md" : ""} ${
-                            isSmall ? "!py-1 !text-body-sm" : ""
-                        }`}
+                            } ${isMedium ? "!py-1.5 !text-body-md" : ""} ${isSmall ? "!py-1 !text-body-sm" : ""
+                            }`}
                         key={option.id}
                         value={option}
                     >
                         {option.name}
                         <Icon
-                            className={`absolute top-1/2 right-3 -translate-y-1/2 opacity-0 transition-opacity group-[[data-selected]]:opacity-100 ${
-                                isMedium || isSmall
+                            className={`absolute top-1/2 right-3 -translate-y-1/2 opacity-0 transition-opacity group-[[data-selected]]:opacity-100 ${isMedium || isSmall
                                     ? "!right-1.5 !size-3.5"
                                     : ""
-                            }`}
+                                }`}
                             name="check"
                         />
                     </ListboxOption>
